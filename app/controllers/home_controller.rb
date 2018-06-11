9912ad7e67a99d04845c1479cba305dddcd7fab6
Monica Howard <jay.m.howard@gmail.com>
2018-06-10 23:29:43 -0500

    Created connection to API in home_controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,14 @@
 class HomeController < ApplicationController
   def index
+
+    require 'net/http'
+    require 'json'
+
+    @url = 'https://api.coinmarketcap.com/v2/ticker/'
+    @url = URI(@url)
+    @response = Netgit status::HTTP.get(@url)
+    @coins = JSON.parse(@response)
+
   end
 
   def about

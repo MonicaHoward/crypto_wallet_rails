134c18bffbd8894883634b2ac5bffff44f79fa17
Monica Howard <jay.m.howard@gmail.com>
2018-06-11 22:36:27 -0500

    Hooked up search box to API

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,21 +1,34 @@
 class HomeController < ApplicationController
   def index
-
     require 'net/http'
     require 'json'
 
-    @url = 'https://api.coinmarketcap.com/v2/ticker/'
+    @url = 'https://api.coinmarketcap.com/v1/ticker/'
     @url = URI(@url)
     @response = Net::HTTP.get(@url)
     @coins = JSON.parse(@response)
-
   end
 
   def about
-
   end
 
   def search
+    require 'net/http'
+    require 'json'
+
+    @url = 'https://api.coinmarketcap.com/v1/ticker/'
+    @url = URI(@url)
+    @response = Net::HTTP.get(@url)
+    @coin_search = JSON.parse(@response)
+
    @symbol = params[:sym]
+    if @symbol
+      @symbol = @symbol.upcase
+    end
+
+    if @symbol == ""
+      @symbol = "Please Enter a Currency"
+    end
   end
+
 end

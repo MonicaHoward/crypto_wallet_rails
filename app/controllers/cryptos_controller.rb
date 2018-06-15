aaa1baefb6aa61bd6664603a33beb4ff2cd65118
Monica Howard <jay.m.howard@gmail.com>
2018-06-14 22:50:35 -0500

    Created edit and new views for cryptos controller

--- app/controllers/cryptos_controller.rb
+++ app/controllers/cryptos_controller.rb
@@ -1,2 +1,12 @@
 class CryptosController < ApplicationController
+  before_action :set_crypto, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
+
+  def index
+    @cryptos = Crypto.all
+  end
+
+  def new
+    @crypto = Crypto.new
+  end
 end

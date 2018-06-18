e71f8c2da567840b740cb5cd8946d1f4d2808e83
Monica Howard <jay.m.howard@gmail.com>
2018-06-17 23:25:09 -0500

    Added actions to cryptos controller

--- app/controllers/cryptos_controller.rb
+++ app/controllers/cryptos_controller.rb
@@ -1,6 +1,7 @@
 class CryptosController < ApplicationController
   before_action :set_crypto, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
 
   def index
     @cryptos = Crypto.all
@@ -13,5 +14,38 @@ class CryptosController < ApplicationController
     @crypto = Crypto.new
   end
 
+  def edit
+
+  end
+
+  def create
+    @crypto = Crypto.new(crypto_params)
+
+    respond_to do |format|
+    if @crypto.save
+      format.html { redirect_to @crypto, notice: 'Crypto was successfully saved'}
+      format.json { render :show, status: :ok, location: @crypto }
+    else
+      format.html { render :edit }
+      format.json { render json: @crypto.errors, status: :unprocessable_entity }
+    end
+    end
+end
+
+  private
+
+  def set_crypto
+    @crypto = Crypto.find(params [:id])
+
+  end
+
+  def crypto_params
+    params.require(:crypto).permit(:symbol, :user_id, :cost_per, :amount_purchased )
+  end
+
+  def correct_user
+    @correct = current_user.cryptos.find_by(id: params[:id])
+    redirect_to cryptos_path, notice: "Not authorized to perform action"
+  end
 
 end
